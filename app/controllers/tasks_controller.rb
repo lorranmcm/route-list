a4430447c233e34be3193039ca02d9f4a60fe53b
Pilar Figueroa <pifigueroa7@gmail.com>
2021-11-30 14:18:48 -0300

    Create tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,21 @@
 class TasksController < ApplicationController
+  def create
+  @project = Project.find(params[:project_id])
+  Task.where(project: @project)
+  @task = Task.new
+  authorize @task
+  @task.user = current_user
+  @task.project = @project
+  @task.status = false
+  @task.description = params[:description]
+  @task.address = params[:address]
+  @task.order = params[:order]
+
+  @project.tasks.each do |t|
+    if t != @task && t.order <= @task.order
+    t.order -= 1
+    t.save!
+  end
+  @task.save
+end
 end
