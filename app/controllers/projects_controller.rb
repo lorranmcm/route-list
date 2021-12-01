f657ae0a9454057bcd3e1cbc8a2fa624e1f06608
Abdelrahman Alshater <shater_11@hotmail.com>
2021-12-01 10:50:45 -0300

    done with the create method of project

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,6 +2,21 @@ class ProjectsController < ApplicationController
   def index
     @projects = Project.all
     @projects = policy_scope(Project).order(created_at: :desc)
+    @project = Project.new
   end
 
+  def create
+    @project = Project.new(project_params)
+    @user = current_user
+    @project.user = @user
+    authorize @project
+    @project.save
+    redirect_to projects_path
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:title)
+  end
 end
