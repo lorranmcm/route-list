7f174cbfb110a79bb49604d7f228792ced4b0dbf
Lorran Monteiro <lorranmcm@gmail.com>
2021-12-03 14:51:03 -0300

    seeds for presentation

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,33 +6,38 @@ User.destroy_all
 puts "DB clean"
 
 user_seed = User.new(
-  first_name: "Lorran",
-  last_name: "Monteiro",
-  email: "lorranmcm@gmail.com",
+  first_name: "Zippora",
+  last_name: "Latzko",
+  email: "zippy@gmail.com",
   password: "123456"
 )
 user_seed.save!
 puts "User Created"
 
-counter = 0
+project_seed_one = Project.new(title: "Demo day")
+project_seed_one.user = user_seed
+project_seed_one.save!
 
-5.times do
-  counter += 1
+project_seed_two = Project.new(title: "Happy hour")
+project_seed_two.user = user_seed
+project_seed_two.save!
 
-  project_seed = Project.new(
-    title: "Project #{counter}",
-  )
-  project_seed.user = user_seed
-  project_seed.save!
+puts "Created Projects"
 
-  puts "Created Project"
+task_seed_one = Task.new(
+  title: "Get documents on Le Wagon",
+  description: "Milene is waiting for us to catch some documents to deliver in the Correios",
+  address: "R. Alm. Gonçalves, 5 - Copacabana, Rio de Janeiro"
+)
+task_seed_one.project = project_seed_one
+task_seed_one.save!
 
-  task_seed = Task.new(
-    title: "Task #{counter}",
-    description: "Lorem Ipsum",
-  )
-  task_seed.project = project_seed
-  task_seed.save!
+task_seed_two = Task.new(
+  title: "Pick up Pilar and go to Mark`s kissing arena",
+  description: "After the delivery of the documents on Correios, give a ride to Pilar to Mark`s kissing arena.",
+  address: "Avenida Nossa Senhora de Copacabana, 1236"
+)
+task_seed_two.project = project_seed_one
+task_seed_two.save!
 
-  puts "Created Task"
-end
+puts "Created Task"
