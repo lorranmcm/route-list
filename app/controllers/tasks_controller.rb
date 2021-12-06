ff88c8040bb31e36abc8cc08b4a7282885f65984
Abdelrahman Alshater <shater_11@hotmail.com>
2021-12-06 11:31:43 -0300

    added taask id to index task controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,6 +6,7 @@ class TasksController < ApplicationController
     @tasks = policy_scope(Task).order(created_at: :desc)
     @markers = @tasks.geocoded.map do |task|
       {
+        task_id: task.id,
         lat: task.latitude,
         lng: task.longitude,
         info_window: render_to_string(partial: "info_window", locals: { task: task })
