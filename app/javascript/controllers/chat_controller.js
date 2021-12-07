5205e6addb4736075c5b80cf706f56c7bc154bb9
Zippora Latzko <zlatzko@googlemail.com>
2021-12-07 17:38:51 -0300

    resetting value of message

--- app/javascript/controllers/chat_controller.js
+++ app/javascript/controllers/chat_controller.js
@@ -8,13 +8,14 @@ export default class extends Controller {
     fetch(`/chatrooms/${chatId}`, { headers: { 'Accept': 'text/plain' } })
       .then(response => response.text())
       .then((data) => {
-        console.log(data)
+        console.log(data);
         this.chatboxTarget.innerHTML = data;
         const messagesContainer = document.getElementById('messages');
-
         consumer.subscriptions.create({ channel: "ChatroomChannel", id: chatId }, {
           received(data) {
             messagesContainer.insertAdjacentHTML('beforeend', data);
+            const formContainer = document.getElementById('message_content');
+            formContainer.value = '';
           }
         });
       })
