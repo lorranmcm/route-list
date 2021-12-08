e7df5ab82f908eb12b79d80979aac5473eeff092
Lorran Monteiro <lorranmcm@gmail.com>
2021-12-08 17:48:23 -0300

    removed error on error pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -31,12 +31,4 @@ class ApplicationController < ActionController::Base
       format.all  { render nothing: true, status: 404 }
     end
   end
-
-  def server_error
-    respond_to do |format|
-      format.html { render template: 'errors/internal_server_error', layout: 'layouts/error', status: 500 }
-      format.all  { render nothing: true, status: 500}
-    end
-  end
-
 end
